bc619ee482aefc89c67f35cf2e3e857f5c887a14
unknown <jluiscanton@gmail.com>
2021-05-05 10:44:23 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import MainNavComponent from './components/MainNavComponents'
+
+function App() {
+  return (
+    <div className="App">
+      <MainNavComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/MainNavComponents.js
@@ -0,0 +1,14 @@
+function MainNavComponent (){
+    return(
+        <div className="main-nav-big-container">
+            <div className="brand">CYAN BLUE NOW!</div>
+            <ul className="main-nav-content">
+                <li>Work</li>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
+        </div>
+    );
+}
+
+export default  MainNavComponent;
